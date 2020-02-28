488de891de707135f292d3e0d4b07885923d2113
Gabriele Bavastro <gabrieleb84@hotmail.it>
2020-02-28 12:18:15 +0100

    Upload Fase1 Java and JS.

---
+++ src/Fase1.java
@@ -0,0 +1,8 @@
+public class Fase1 {
+	    public static void main(String[] args) {
+	    char[] fullName = {'B','A','V','A','S','T','R','O',' ','N','A','V','O','N','E',' ','G','A','B','R','I','E','L','E'};
+	    for (char i : fullName) {
+	      System.out.println(i);
+	    }    
+	  }
+}
